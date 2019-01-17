e6f042157eef343dff425b62059666d43fe1f39a
some guy <39282879+the-maestro@users.noreply.github.com>
2019-01-17 21:54:17 +0000

    make it work

--- app.js
+++ app.js
@@ -185,14 +185,6 @@ client.on('guildMemberAdd', member => {
       rChannel.send(embed);
 });
 
-//load commands
-const commandFiles = fs.readdirSync('commands/').filter(file => file.endsWith('.js')); // get command files
-for (const file of commandFiles) { // for each command file, require it
-  const command = require(`./commands/${file}`);
-  client.commands.set(command.name, command);
-  console.log(`Loading ${command.name}`);
-}
-
 // database configuration
 client.db.run("CREATE TABLE IF NOT EXISTS calendar (guild TEXT, events TEXT, notifs INTEGER, channel TEXT)");
 setInterval(function() { // goes through each server and its events to check if reminders should be sent
