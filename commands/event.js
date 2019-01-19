7978dc2e225d743ceab20e9ed21ca8605a35c139
some guy <39282879+the-maestro@users.noreply.github.com>
2019-01-19 03:24:34 +0000

    event.js

--- commands/event.js
+++ commands/event.js
@@ -336,23 +336,39 @@ module.exports.run = (client, msg, args) =>{
 
               case emojis.SKULL: // if the skull is clicked
               var toDel = msg.id;
-              toDel.delete();
-              msg.channel.send("Event deleted...")
-              .then(msg =>{
-                msg.delete(10000)
-              })
-                client.db.get(`SELECT events FROM calendar WHERE guild = ${msg.guild.id}`, (err, row) => {
-                  var json = JSON.parse(row.events);
-                  json.list = json.list.filter((event) => { // filter out the current array of events to exclude the array that will be deleted
-                    if (event.id !== toDel) {
-                      return event;
-                    }
-                  });
-                  var insert = JSON.stringify(json); // updated array
-                  console.log(typeof insert);
-                  client.db.run(`UPDATE calendar SET events = ? WHERE guild = ?`, [insert, msg.guild.id], (err) => {
-                  });
+              var toDelFull;
+              client.db.get(`SELECT events FROM calendar WHERE guild = ${msg.guild.id}`, (err, row) => {
+                if (err) { // if an error occurs
+                  console.log("no the error is here");
+                  console.error("Delete.js selection error: ", err.message);
+                  msg.channel.send(new client.discord.RichEmbed().setColor(client.color).setDescription("❗️This server has no events to delete!"));
+                }
+                if (!row) { // if the server does not have any events
+                  msg.channel.send(new client.discord.RichEmbed().setColor(client.color).setDescription("❗️This server has no events to delete!"));
+                }
+                var json = JSON.parse(row.events);
+                json.list = json.list.filter((event) => { // filter out the current array of events to exclude the array that will be deleted
+                  if (event.id !== toDel) {
+                    return event;
+                  }
+                  else {
+                    toDelFull = event;
+                  }
                 });
+                var insert = JSON.stringify(json); // updated array
+                console.log(typeof insert);
+                client.db.run(`UPDATE calendar SET events = ? WHERE guild = ?`, [insert, msg.guild.id], (err) => {
+                  if (err) {
+                    console.error("Delete.js update error: ", err.message);
+                  }
+                  else {
+                    msg.channel.fetchMessage(toDel).then(m => {
+                      m.delete();
+                    });
+                    msg.channel.send(new client.discord.RichEmbed().setColor(client.color).setDescription(`🗑 Event \`${toDel}: ${toDelFull.name}\` has been deleted!`)).delete(10000);
+                  }
+                });
+              });
               break;
             }
         });
