f6ff57d2cb6ad87955ff1f472a554a0a971ac3f2
some guy <39282879+the-maestro@users.noreply.github.com>
2019-01-20 23:12:17 +0000

    changes

--- commands/event.js
+++ commands/event.js
@@ -367,14 +367,6 @@ module.exports.run = async (client, msg, args) =>{
           case emojis.SKULL:
             var toDel = m.id;
             client.db.get(`SELECT events FROM calendar WHERE guild = ${msg.guild.id}`, (err, row) => {
-              if (err) { // if an error occurs
-                console.log("no the error is here");
-                console.error("Delete.js selection error: ", err.message);
-                msg.channel.send(new client.discord.RichEmbed().setColor(client.color).setDescription("❗️This server has no events to delete!"));
-              }
-              if (!row) { // if the server does not have any events
-                msg.channel.send(new client.discord.RichEmbed().setColor(client.color).setDescription("❗️This server has no events to delete!"));
-              }
               var json = JSON.parse(row.events);
               json.list = json.list.filter((event) => { // filter out the current array of events to exclude the array that will be deleted
                 if (event.id !== toDel) {
@@ -384,12 +376,7 @@ module.exports.run = async (client, msg, args) =>{
               var insert = JSON.stringify(json); // updated array
               console.log(typeof insert);
               client.db.run(`UPDATE calendar SET events = ? WHERE guild = ?`, [insert, msg.guild.id], (err) => {
-                if (err) {
-                  console.error("Delete.js update error: ", err.message);
-                }
-                else {
                   m.delete();
-                  }
                 });
               });
               break;
