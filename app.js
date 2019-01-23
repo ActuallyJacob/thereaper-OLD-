66086009f3b1843d326635c79c14a14d722ec965
some guy <39282879+the-maestro@users.noreply.github.com>
2019-01-23 23:03:11 +0000

    warn.js

--- app.js
+++ app.js
@@ -64,6 +64,22 @@ client.on("message", message => {
     if (!message.content.startsWith(config.prefix)){
       client.users.get(config.ownerID).send(`Message Dm'd by: ${message.author.id}, ${message.author.username}, content: ${message.content}`);
     }else{
+      const args = message.content.split(" ").slice(1);
+ 
+      if (message.content.startsWith(config.prefix + "eval")) {
+        if(message.author.id !== config.ownerID) return;
+        try {
+          const code = args.join(" ");
+          let evaled = eval(code);
+     
+          if (typeof evaled !== "string")
+            evaled = require("util").inspect(evaled);
+     
+          message.channel.send(clean(evaled), {code:"xl"});
+        } catch (err) {
+          message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+        }
+      }
       return;
     }
   }else{
